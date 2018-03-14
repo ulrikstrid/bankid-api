46f53007e1278efb74f4e58d93ac3c0af584aee3
Ulrik Strid <ulrik.strid@outlook.com>
2018-03-14 20:08:13 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,88 @@
+"use strict";
+import * as fs from "fs";
+import * as https from "https";
+import * as path from "path";
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+
+const testCertPath = path.resolve("./FPTestcert2_20150818_102329.pfx");
+const testPassphrase = "querty123";
+
+interface ClientOptions {
+  certPath: string;
+  passphrase: string;
+}
+
+const createClient = (opts: ClientOptions) => {
+  const { certPath = testCertPath, passphrase = testPassphrase } = opts;
+  const pfx = fs.readFileSync(certPath);
+
+  return axios.create({
+    httpsAgent: new https.Agent({
+      pfx,
+      passphrase,
+      rejectUnauthorized: false
+    })
+  });
+};
+
+const collect = (client: AxiosInstance, orderRef: string) =>
+  client.post(
+    "https://appapi2.test.bankid.com/rp/v5/collect",
+    {
+      orderRef
+    },
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+const startAuthentication = (
+  client: AxiosInstance,
+  personalNumber: string,
+  endUserIp: string,
+  requirement = {}
+) =>
+  client.post(
+    "https://appapi2.test.bankid.com/rp/v5/auth",
+    {
+      personalNumber,
+      endUserIp,
+      requirement
+    },
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+const authenticate = (
+  client: AxiosInstance,
+  personalNumber: string,
+  endUserIp: string,
+  requirement = {}
+) => {
+  startAuthentication(client, personalNumber, endUserIp, requirement).then(
+    (response: AxiosResponse<{ orderRef: string }>) => {
+      const { data: { orderRef } } = response;
+      return new Promise((resolve, reject) => {
+        // Start a interval to poll every 2 seconds
+        const intervalId = setInterval(() => {
+          collect(client, orderRef)
+            .then(({ data }) => {
+              if (data.status === "complete") {
+                clearInterval(intervalId);
+                resolve(data);
+              } else if (data.status === "failed") {
+                clearInterval(intervalId);
+                reject(data);
+              }
+            })
+            .catch(reject);
+        }, 2000);
+      });
+    }
+  );
+};
